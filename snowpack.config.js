110efb419555b4f63e32b305efcaaeb44873589f
Jeremias Erbs <jerry@badabam.de>
2021-02-09 00:49:19 +0100

    Remove webpack plugin

--- snowpack.config.js
+++ snowpack.config.js
@@ -2,33 +2,18 @@
 // See all supported options: https://www.snowpack.dev/reference/configuration
 
 /** @type {import("snowpack").SnowpackUserConfig } */
+
 module.exports = {
   mount: {
     src: '/',
   },
-  plugins: [
-    [
-      '@snowpack/plugin-webpack',
-      {
-        sourceMap: true,
-        htmlMinifierOptions: {
-          collapseWhitespace: true,
-          removeComments: true,
-          removeEmptyAttributes: true,
-          removeRedundantAttributes: true,
-          removeScriptTypeAttributes: true,
-          removeStyleLinkTypeAttributes: true,
-        },
-      },
-    ],
-  ],
-  packageOptions: {
-    /* ... */
+  optimize: {
+    bundle: true,
+    minify: true,
+    target: 'es2017',
   },
-  devOptions: {
-    /* ... */
-  },
-  buildOptions: {
-    /* ... */
-  },
+  plugins: [],
+  packageOptions: {},
+  devOptions: {},
+  buildOptions: {},
 }
