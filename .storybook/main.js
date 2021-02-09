9c4efc0c2219a23493ba235f44c8fb2196d8d547
Jeremias Erbs <jerry@badabam.de>
2021-02-09 01:07:47 +0100

    Turn of storybook controls

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,7 +8,7 @@ module.exports = {
     {
       name: '@storybook/addon-essentials',
       options: {
-        controls: true,
+        controls: false,
         actions: true,
         docs: false,
       },
